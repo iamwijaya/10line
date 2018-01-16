e1b4bab96d85e03a16e29c9777f7c19b36ef9a5f
iamwijaya <31705906+iamwijaya@users.noreply.github.com>
2018-01-17 04:28:31 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -39,7 +39,7 @@ class LINE:
     self._headers = {
               'X-Line-Application': 'CHROMEOS	2.0.0	Chrome_OS	1',#'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0', 
               'X-Line-Access': self.authToken, 
-              'User-Agent': 'Line/20.0.0'#'Line/7.18.0 AppleWebKit/534.30'#'Line/6.0.0 iPad4,1 9.0.2'#'Line/7.18.0 iPad4,1 9.0.2'
+              'User-Agent': 'Line/8.0.0'#'Line/7.18.0 AppleWebKit/534.30'#'Line/6.0.0 iPad4,1 9.0.2'#'Line/7.18.0 iPad4,1 9.0.2'
    }
 
     self.Poll = Poll(self.authToken)
